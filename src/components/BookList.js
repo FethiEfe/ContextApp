884581e737ca7d0a6c1dd2dcd75be0fe01f5ab02
FETHI AKCAY <f.akcay1@gmail.com>
2021-05-13 01:57:04 -0400

    using useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,41 @@
-import React, {Component} from "react"
+import React, {Component, useContext} from "react"
+import { BookContext } from "../contexts/BookContext"
 import { ThemeContext } from "../contexts/ThemeContext"
 
-class BookList extends Component {
-    static contextType = ThemeContext 
-    // contextType only works in class components
-    // can not use multiple context
-    render(){
-        const {isLightTheme, dark, light} = this.context
-        const theme = isLightTheme ? light : dark
-        return(
-            <div className = "book-list" style={{background:theme.bg, color: theme.syntax}}>
-                <ul>
-                    <li style = {{background: theme.ui}}>the way of kings</li>
-                    <li style = {{background: theme.ui}}>the name of the wind</li>
-                    <li style = {{background: theme.ui}}>the final empire</li>
-                </ul>
-            </div>
-        )
-    }
+// class BookList extends Component {
+//     static contextType = ThemeContext 
+//     // contextType only works in class components
+//     // can not use multiple context
+//     render(){
+//         const {isLightTheme, dark, light} = this.context
+//         const theme = isLightTheme ? light : dark
+//         return(
+//             <div className = "book-list" style={{background:theme.bg, color: theme.syntax}}>
+//                 <ul>
+//                     <li style = {{background: theme.ui}}>the way of kings</li>
+//                     <li style = {{background: theme.ui}}>the name of the wind</li>
+//                     <li style = {{background: theme.ui}}>the final empire</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+const BookList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext)
+    const {books} = useContext(BookContext)
+    const theme = isLightTheme ? light : dark
+    return(
+        <div className = "book-list" style={{background:theme.bg, color: theme.syntax}}>
+            <ul>
+                {books.map(book => {
+                    return (
+                        <li key = {book.id} style = {{background: theme.ui}}>{book.title}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+    
 }
 export default BookList
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,57 @@
-import React, {Component} from "react"
+import React, {Component, useContext} from "react"
 import { ThemeContext} from "../contexts/ThemeContext"
 import { AuthContext} from "../contexts/AuthContext"
 
-class Navbar extends Component{
-    render(){
-        return(
-            <AuthContext.Consumer>{(authContext) => (
-                <ThemeContext.Consumer>{(themeContext) => { 
-                    // Consumer can be used in both class and functional component
-                    // Another benefit of using Consumer is consuming multiple context
-                    const {isLightTheme, light,dark} = themeContext
-                    const {isAuthenticated, toggleAuth} = authContext
-                    const theme = isLightTheme ? light : dark
-                    return (
-                        <nav style ={{background: theme.ui, color: theme.syntax}}>
-                            <h1>Context App</h1>
-                            <div onClick = {toggleAuth}>
-                                {isAuthenticated ? "Logged in" : "Logged out"}
-                            </div>
-                            <ul>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
-                    )
-                    }}
-                </ThemeContext.Consumer>
-            )} 
-            </AuthContext.Consumer>
+// class Navbar extends Component{
+//     render(){
+//         return(
+//             <AuthContext.Consumer>{(authContext) => (
+//                 <ThemeContext.Consumer>{(themeContext) => { 
+//                     // Consumer can be used in both class and functional component
+//                     // Another benefit of using Consumer is consuming multiple context
+//                     const {isLightTheme, light,dark} = themeContext
+//                     const {isAuthenticated, toggleAuth} = authContext
+//                     const theme = isLightTheme ? light : dark
+//                     return (
+//                         <nav style ={{background: theme.ui, color: theme.syntax}}>
+//                             <h1>Context App</h1>
+//                             <div onClick = {toggleAuth}>
+//                                 {isAuthenticated ? "Logged in" : "Logged out"}
+//                             </div>
+//                             <ul>
+//                                 <li>Home</li>
+//                                 <li>About</li>
+//                                 <li>Contact</li>
+//                             </ul>
+//                         </nav>
+//                     )
+//                     }}
+//                 </ThemeContext.Consumer>
+//             )} 
+//             </AuthContext.Consumer>
            
-        )
-    }
+//         )
+//     }
+// }
+
+const Navbar = () => {
+    const {isLightTheme, light,dark} = useContext(ThemeContext)
+    const {isAuthenticated, toggleAuth} = useContext(AuthContext)
+    const theme = isLightTheme ? light : dark
+    return (
+        <nav style ={{background: theme.ui, color: theme.syntax}}>
+            <h1>Context App</h1>
+            <div onClick = {toggleAuth}>
+                {isAuthenticated ? "Logged in" : "Logged out"}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )        
 }
+    
 
 export default Navbar
